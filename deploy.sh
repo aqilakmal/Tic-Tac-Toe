a6e1c9025aecf518a42b71fb9bb3112dc8df2d7a
Muhammad Faqih Akmal <62086734+AqilAkmal@users.noreply.github.com>
2022-05-21 19:28:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
 
 git init
 git add -A
-git commit -m 'New Deployment'
-git push -f git@github.com:aqilakmal/Tic-Tac-Toe.git master:gh-pages
+git commit -m 'deploy'
+git push -f git@github.com:aqilakmal/Tic-Tac-Toe.git main:gh-pages
 
 cd -
