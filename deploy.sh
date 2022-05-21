6675466336833de59fde076ec5eda110b1f55d89
Muhammad Faqih Akmal <62086734+AqilAkmal@users.noreply.github.com>
2022-05-21 19:24:53 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:AqilAkmal/Tic-Tac-Toe.git master:gh-pages
+git push -f git@github.com:aqilakmal/Tic-Tac-Toe.git master:gh-pages
 
 cd -
